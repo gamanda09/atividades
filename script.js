da0e43c7032d2f36f7f214b2721efae9173ca69e
Amanda Alves <amandacontatoalves@gmail.com>
2024-12-13 13:14:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".botao").addEventListener("click", function(){
+
+    const paragrafo = document.querySelector(".paragrafo").innerHTML = "Você clicou no botão!"
+   document.querySelector(".paragrafo").innerHTML = "Você clicou no botão!"
+   paragrafo.innerHTML = "Você clicou no botão!"
+   paragrafo.style.color = "aqua"
+})
+
